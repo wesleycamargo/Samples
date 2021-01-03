869de8b86270756776e686132d1efd855ea99eb5
Wesley Camargo <camargo.wes@gmail.com>
2021-01-03 16:02:11 +0100

    Added AppInsights-Telemetry

---
+++ Application-Insights-Telemetry/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using appinsights_custom_event.Models;
+using Microsoft.ApplicationInsights;
+
+namespace appinsights_custom_event.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TelemetryClient telemetryClient;
+
+        public HomeController(TelemetryClient telemetryClient)
+        {
+            this.telemetryClient = telemetryClient;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public ActionResult Hello(string button)
+        {
+            this.telemetryClient.TrackEvent("HelloWorld");
+            ViewBag.Message = "Hello World!";
+            return View("Index");
+        }
+    }
+}
---
+++ Application-Insights-Telemetry/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace appinsights_custom_event.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
